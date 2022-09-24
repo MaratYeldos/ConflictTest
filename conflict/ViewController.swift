5d2a0d90e91fb49dedd7ea439635de481a911cfc
Yeldos <eldos.boom001@gmail.com>
2022-09-24 18:30:05 +0600

    four

--- conflict/ViewController.swift
+++ conflict/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         customView.isHidden = false
         return customView
     }()
+    
+    var customView2: UIView = {
+        var customView = UIView()
+        customView.backgroundColor = .black
+        customView.isHidden = false
+        return customView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +32,14 @@ class ViewController: UIViewController {
             customView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             customView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             customView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100)
+        ])
+        
+        view.addSubview(customView2)
+        NSLayoutConstraint.activate([
+            customView2.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+            customView2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            customView2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            customView2.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100)
         ])
     }
 
