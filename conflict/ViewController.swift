ca7ed08b45296b0a1c12df211fb1a9fdc9c994f9
Yeldos <eldos.boom001@gmail.com>
2022-09-24 17:01:39 +0600

    first

--- conflict/ViewController.swift
+++ conflict/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var customView: UIView = {
+        var customView = UIView()
+        customView.backgroundColor = .red
+        return customView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(customView)
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            customView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            customView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            customView.heightAnchor.constraint(equalToConstant: 100)
+        ])
     }
 
 
