7b47b7fad37b56755ab16d22fb66f9de74fda4e9
Yeldos <eldos.boom001@gmail.com>
2022-09-27 18:09:55 +0600

    sixsix

--- conflict/ViewController.swift
+++ conflict/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
     
     var customView: UIView = {
         var customView = UIView()
-        customView.backgroundColor = .purple
         customView.backgroundColor = .cyan
         return customView
     }()
